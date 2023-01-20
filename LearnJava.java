fa1126053491a5c7bb2447bf8cfac1d84d295f8c
BrianvanWinden <brianvanwinden@hotmail.com>
2023-01-20 09:01:34 +0100

    updates?

---
+++ LearnJava.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class LearnJava{
+
+    public static void main(String[] arg){
+        // Phase 1: 
+        // int a = 4;
+        // String name = "Brian";
+
+
+        // Phase 2
+        // String naam = name.toUpperCase();
+        // System.out.println(naam.toUpperCase()); 
+        
+        // Phase 3, functions (or 'methods'): 
+        // String gekkenaam = addExclamationPoint("Sinaasappel");
+        // System.out.println(gekkenaam);
+
+        // Phase 4: Functions from other files
+        // Animal a = new Animal(); 
+        // String dog =  a.iAmDog();
+        // System.out.println(dog);
+
+
+        ArrayList<Integer> a = new ArrayList<Integer>()
+        a.
+        }
+
+    public static String addExclamationPoint(String s){
+        return s + "!";
+
+    }
+}
